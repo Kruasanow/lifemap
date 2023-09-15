d7be7905c21b244034ef6d76adf7debc92c9fa85
kruasanow <rusanow.mitya@bk.ru>
2023-09-15 23:37:47 +0300

    321

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 # app.py
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for, flash, session
 
 app = Flask(__name__)
+app.secret_key = 'zalupa227' 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/createdescription')
+def createdescription():
+    x = session['X']
+    y = session['Y']
+    return render_template('createdescription.html', x = x, y = y)
 
 @app.route('/save_coordinates', methods=['POST'])
 def save_coordinates():
@@ -14,8 +21,15 @@ def save_coordinates():
     
     # Здесь вы можете обработать или сохранить координаты, как вам нужно
     print(f"X: {x}, Y: {y}")
-    
+    session['X'] = x
+    session['Y'] = y
     return jsonify(status="success")
+
+@app.route('/button_pressed', methods=['POST'])
+def button_pressed():
+    # Здесь выполняется обработка нажатия кнопки
+    flash('Button was pressed!')
+    return redirect(url_for('createdescription'))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5555)
---
+++ static/js/hueta.js
@@ -0,0 +1,11 @@
+<script>
+$(document).ready(function(){
+    $(".textmonial_active").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        items: 1,
+        navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"]
+    });
+});
+</script>
