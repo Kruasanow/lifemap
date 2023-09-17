7cdf1850a4d418ce8859bf916326b04694cb4ac5
kruasanow <rusanow.mitya@bk.ru>
2023-09-17 14:53:48 +0300

    431

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ from werkzeug.utils import secure_filename
 import os
 from flask_bcrypt import Bcrypt
 from init_db import get_db_connection
+from functools import wraps
+from sensetive_data import admin_key, app_secret_key
 
 app = Flask(__name__)
-app.secret_key = 'ebanaya pomoyka'  # Random secret key for sessions
+app.secret_key = app_secret_key  # Random secret key for sessions
 bcrypt = Bcrypt(app)
 app.config['UPLOAD_FOLDER'] = 'uploads/'  # Папка для загрузки файлов
 
-from functools import wraps
+ADMIN_KEY = admin_key
 
 def login_required(f):
     @wraps(f)
@@ -19,6 +21,15 @@ def login_required(f):
         if 'loggedin' not in session:
             flash('Please log in to access this page.', 'warning')
             return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
+def admin_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('is_admin', False):
+            flash('You need to be an admin to access this page.', 'danger')
+            return redirect(url_for('main_2'))
         return f(*args, **kwargs)
     return decorated_function
 
@@ -63,9 +74,9 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        admin_key = request.form.get('admin_key', '')
 
-        conn = get_db_connection()  # Использование функции для установки соединения
-        
+        conn = get_db_connection()
         cur = conn.cursor()
         try:
             cur.execute("SELECT passwd FROM users WHERE username = %s", (username,))
@@ -75,13 +86,20 @@ def login():
         finally:
             cur.close()
             conn.close()
-        print(stored_password)
+
         if stored_password and bcrypt.check_password_hash(stored_password[0], password):
             session['loggedin'] = True
             session['username'] = username
 
-            flash('Logged in successfully!', 'success')
-            return redirect(url_for('index'))  # Перенаправление на главную страницу
+            # Проверка на наличие прав админа
+            if admin_key == ADMIN_KEY:
+                session['is_admin'] = True
+                flash('Logged in as admin successfully!', 'success')
+                return redirect(url_for('main'))
+            else:
+                session['is_admin'] = False
+                flash('Logged in successfully!', 'success')
+                return redirect(url_for('main_2'))
 
         else:
             flash('Login failed! Check your username and password.', 'danger')
@@ -90,6 +108,17 @@ def login():
     return render_template('login.html')
 
 
+
+@app.route('/main')
+@login_required
+@admin_required
+def main():
+    return render_template('main.html')
+
+@app.route('/main_2')
+@login_required
+def main_2():
+    return render_template('main_2.html')    
 
 @app.route('/')
 @login_required
@@ -104,12 +133,12 @@ def createdescription():
     if form.validate_on_submit():
         title = form.title.data
         short_description = form.short_description.data
-
-        # Выводим в консоль
-        print(title, short_description)
+        full_description = form.full_story.data
 
         # Создаем основную директорию для события
-        event_folder = os.path.join(app.config['UPLOAD_FOLDER'], title)
+        user_folder = os.path.join(app.config['UPLOAD_FOLDER'], session['username'])
+        event_folder = os.path.join(user_folder, title)
+
         if not os.path.exists(event_folder):
             os.makedirs(event_folder)
 
@@ -118,9 +147,14 @@ def createdescription():
         if f and f.filename and allowed_file(f.filename):
             filename = secure_filename(f.filename)
             f.save(os.path.join(event_folder, filename))
+            photo_path = os.path.join(title, filename)  # Путь к сохраненному фото
+
+        else:
+            photo_path = None
 
         # Сохраняем галерею фото, если они есть
         gallery_photos = request.files.getlist('multiple_photos')
+        gallery_paths = []
         if gallery_photos:
             gallery_folder = os.path.join(event_folder, "галерея фото")
             if not os.path.exists(gallery_folder):
@@ -130,6 +164,24 @@ def createdescription():
                 if upload and upload.filename and allowed_file(upload.filename):
                     filename = secure_filename(upload.filename)
                     upload.save(os.path.join(gallery_folder, filename))
+                    gallery_paths.append(os.path.join(title, "галерея фото", filename))  # Пути к сохраненным фото
+
+        else:
+            gallery_paths = []
+
+        coords = session['X'] + ':' + session['Y']
+
+        # Вставляем данные в базу данных
+        conn = get_db_connection()
+        cur = conn.cursor()
+
+        cur.execute('INSERT INTO events (coords, title, short_description, full_description, photo, gallery_photos, owner_name) '
+                    'VALUES (%s, %s, %s, %s, %s, %s, %s)',
+                    (coords, title, short_description, full_description, photo_path, gallery_paths, session['username']))
+
+        conn.commit()
+        cur.close()
+        conn.close()
 
         return redirect(url_for('createdescription'))
 
