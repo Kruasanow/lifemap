54a905f5160f9abe37718910634ce6fb5de63e1e
kruasanow <rusanow.mitya@bk.ru>
2023-10-19 00:05:13 +0300

    123123

---
+++ install.sh
@@ -0,0 +1,7 @@
+pip3 install --upgrade pip
+sudo usermod -aG `whoami` postgres
+sudo -u postgres createdb $DBNAME
+sudo -u postgres psql -c "CREATE USER $DBUSER with password '$DBPASSWORD';"
+sudo -u postgres psql -d $DBNAME -c "GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO $DBUSER;"
+python3 init_db.py
+sudo -u postgres psql -d $DBNAME -c "INSERT INTO maps (city,city_name,filename) VALUES ('spb','Saint-Petersburg','spb.png');"
