052802cf99f3f1c11be069d31cb36ab73d778f94
kruasanow <rusanow.mitya@bk.ru>
2024-01-06 00:06:03 +0300

    dsa

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 sudo usermod -aG `whoami` postgres
-# sudo -u postgres createdb $DBNAME
-sudo -u postgres createdb $DBNAMEPRIVATE
+sudo -u postgres createdb $DBNAME
+#sudo -u postgres createdb $DBNAMEPRIVATE
 sudo -u postgres psql -c "CREATE USER $DBUSER with password '$DBPASSWORD';"
 # sudo -u postgres psql -c "CREATE USER $DBUSERPRIVATE with password '$DBPASSWORDPRIVATE';"
 sudo -u postgres psql -d $DBNAME -c "GRANT ALL PRIVILEGES ON DATABASE $DBNAME TO $DBUSER;"
 # sudo -u postgres psql -d $DBNAMEPRIVATE -c "GRANT ALL PRIVILEGES ON DATABASE $DBNAMEPRIVATE TO $DBUSERPRIVATE;"
 python3 init_db.py
-sudo -u postgres psql -d $DBNAME -c "INSERT INTO maps (city,city_name,filename) VALUES ('spb','Saint-Petersburg','spb.png');"
-# sudo -u postgres psql -d $DBNAMEPRIVATE -c "INSERT INTO maps (city,city_name,filename) VALUES ('spb','Saint-Petersburg','spb.png');"
+sudo -u postgres psql -d $DBNAME -c "INSERT INTO maps (city,city_name,filename) VALUES ('spb','Санкт-Петербург','spb.png');"
+sudo -u postgres psql -d $DBNAME -c "INSERT INTO maps (city,city_name,filename) VALUES ('bg','Белая Глина','bg.png');"
 
 # VSE NADO EBASHIT V COMPOSE YA TOGO ROT EBAL
